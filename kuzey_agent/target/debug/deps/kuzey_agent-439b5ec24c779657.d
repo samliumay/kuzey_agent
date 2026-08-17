8a8d92ab64fa5d8a8c27db6703de278332983a6a
samliumay <samliumay965@gmail.com>
2026-08-17 16:47:24 +0300

    base of the project.

---
+++ kuzey_agent/target/debug/deps/kuzey_agent-439b5ec24c779657.d
@@ -0,0 +1,5 @@
+/home/umay-samli/Desktop/projects/kuzey_agent/kuzey_agent/target/debug/deps/kuzey_agent-439b5ec24c779657.d: src/main.rs
+
+/home/umay-samli/Desktop/projects/kuzey_agent/kuzey_agent/target/debug/deps/libkuzey_agent-439b5ec24c779657.rmeta: src/main.rs
+
+src/main.rs:
---
+++ kuzey_agent/target/debug/deps/kuzey_agent-4e5f7f1fdb93dcf6.d
@@ -0,0 +1,5 @@
+/home/umay-samli/Desktop/projects/kuzey_agent/kuzey_agent/target/debug/deps/kuzey_agent-4e5f7f1fdb93dcf6.d: src/main.rs
+
+/home/umay-samli/Desktop/projects/kuzey_agent/kuzey_agent/target/debug/deps/libkuzey_agent-4e5f7f1fdb93dcf6.rmeta: src/main.rs
+
+src/main.rs:
